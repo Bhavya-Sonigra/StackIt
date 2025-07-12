c48f941f3bc32a2be3f19afcf82e2811724d2499
Bhavya Sonigra <sonigrabhavya28@gmail.com>
2025-07-12 11:53:38 +0530

    Finalized project structure: separated client and server

--- utils/token.js
+++
@@ -1,7 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-export const signAccess  = (payload) =>
-  jwt.sign(payload, process.env.JWT_ACCESS_SECRET,  { expiresIn: process.env.ACCESS_TTL });
-
-export const signRefresh = (payload) =>
-  jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: process.env.REFRESH_TTL });
